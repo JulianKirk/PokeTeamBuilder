dac572fe5384ad96c7f3f2131aead23d98b5ef77
Julian <progamerguy005@gmail.com>
2022-10-12 23:45:41 +1100

    Added most of the functionality with the name options. Options come out as undefined but console manages to show them. Suspect that it is because getPokemon is done asyncronously or something

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+root = document.querySelector("#root")
 
-function getPokemon(name) //Name is case sensitive (full lowercase only) 
+function getPokemon(name, outputFunc) //Name is case sensitive (full lowercase only) 
 {
     fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
     .then((response) => response.json())
-    .then((data) => (displayPokemon(data)))
+    .then((data) => (outputFunc(data)))
     .catch((err) => console.log("Pokemon not found", err));
 }
 
@@ -14,8 +13,27 @@ function displayPokemon(data)
     console.log(data);
 }
 
-function initializeOptions() {
-    
+function returnPokeName(data) {
+    console.log(data.name);
+    return data.name;
 }
 
-getPokemon("ditto");
+function initializeOptions() {
+    fetch(`https://pokeapi.co/api/v2/pokemon-species/?limit=0`)
+    .then(response => response.json())
+    .then((data) => {
+        for (let i = 1; i < data.count; i++) 
+        {
+            document.querySelector("#PokemonNameSelector").innerHTML +=
+            `
+                <Option>${getPokemon(i, returnPokeName)}</Option>
+            `
+            ; 
+        }
+    })
+    .catch((err) => console.log("Pokemon count cannot be found", err));
+}
+
+getPokemon("ditto", displayPokemon);
+initializeOptions();
+console.log(document.querySelector("#PokemonNameSelector").innerHTML)
