00c7be3ff87691bb909a38147e4b83319ed87fca
JulianKirk <progamerguy005@gmail.com>
2023-02-25 23:38:32 +1100

    Fixed all the errors with getting the pokemon details updating. About to attempt to get rid of unecessarily repetitive stuff

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-PokemonOne = document.querySelector("#PokemonOne")
+PokemonOne = document.querySelector("#PokemonOne");
+PokemonTwo = document.querySelector("#PokemonTwo");
 
 function getPokemon(name, outputFunc) //Name is case sensitive (full lowercase only) 
 {
@@ -14,24 +15,30 @@ function updateContent(currentPokemon) {
     let pokeImage = currentPokemon.querySelector("#PokemonImage");
     let nameSelectBox = currentPokemon.querySelector("#PokemonNameSelector");
 
-    let pokeName = nameSelectBox.options[nameSelectBox.selectedIndex]?.value;
-    console.log(pokeName);
-    pokeName = pokeName == undefined ? "bulbasaur" : pokeName; //WHY WON'T ANYTHING BUT BULBASAUR WORK
-    console.log(pokeName);
+    let pokeName = nameSelectBox.value ?? "bulbasaur";
 
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
             .then((response) => response.json())
             .then(function(data) {
+                let types = ""
+
+                if(data.types.length == 1) 
+                {
+                    types = data.types[0].type.name
+                } else 
+                {
+                    types = `${data.types[0].type.name}, ${data.types[1].type.name}`
+                }
+
                 content.innerHTML =
                 `<p>
                 Name: ${pokeName} <br>
-                Type(s): ${data.types[0].type.name}; ${data.types[1].type?.name ?? ""} <br>
+                Type(s): ${types} <br>
                 Weaknesses: example weaknesses
                 </p>`; //Later change the types to show on the box graphic
                 
                 pokeImage.innerHTML = 
-                `<image src = "${data.sprites.front_default}" alt = "Image of the pokemon"></image>
-                ;`
+                `<image src = "${data.sprites.front_default}" alt = "Image of the pokemon"></image>`
                 
             })
             .catch((err) => console.log("Pokemon not found", err));
@@ -42,24 +49,56 @@ function updateContent(currentPokemon) {
 function initializeOptions(currentPokemon) {
     fetch(`https://pokeapi.co/api/v2/pokemon-species`) //Removed the "?limit=0"
     .then(response => response.json())
-    .then((data) => { //There was an "async" here before but i'm not sure why        
+    .then(async (data) => { 
+        let promises= [];  
         for (let i = 1; i < data.count; i++) 
         {
-            fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
-            .then((response) => response.json())
-            .then(function(data) {
-                currentPokemon.querySelector("#PokemonNameSelector").innerHTML +=
-                `<Option value = "${data.name}">${data.name}</Option>`;
-            })
-            .catch((err) => console.log('Pokemon' + i + 'cannot be found/added to the list', err));
+            let promise = fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+                .then((response) => response.json())
+                .then(function(data) {
+                    currentPokemon.querySelector("#PokemonNameSelector").innerHTML +=
+                    `<Option value = "${data.name}">${data.name}</Option>`;
+                })
+                .catch((err) => console.log('Pokemon' + i + 'cannot be found/added to the list', err));
+            promises.push(promise);
         }
+        await Promise.all(promises);
+        updateContent(currentPokemon);
     })
-    //.then(updateContent(currentPokemon)) //NOT SURE IF THIS IS EVEN RUNNING
     .catch((err) => console.log("Pokemon count cannot be found!", err)); 
 }
 
+async function obtainOptions()
+{
+    let promises = []
+
+    let pokemonNameOptions = []
+    fetch(`https://pokeapi.co/api/v2/pokemon-species`) //Removed the "?limit=0"
+    .then(response => response.json())
+    .then((data) => { 
+        for (let i = 1; i < data.count; i++) 
+        {
+            let promise = fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+                .then((response) => response.json())
+                .then(function(data) { pokemonNameOptions.push(data.name)})
+                .catch((err) => console.log('Pokemon' + i + 'cannot be found/added to the list', err));
+            promises.push(promise);
+        }
+    })
+
+    let attackOptions = []
+
+    await Promise.all(promises);
+
+    initializeOptions(PokemonOne);
+    initializeOptions(PokemonTwo);
+}
+
 //#region Detect Changes / Events
-    PokemonOne.querySelector("#PokemonNameSelector").onchange = updateContent(PokemonOne); //This is not working for some reason
+    PokemonOne.querySelector("#PokemonNameSelector").onchange = () => updateContent(PokemonOne);
+    PokemonTwo.querySelector("#PokemonNameSelector").onchange = () => updateContent(PokemonTwo);
 //#endregion
 
+// obtainOptions();
 initializeOptions(PokemonOne);
+
